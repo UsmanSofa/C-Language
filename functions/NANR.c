0704a18d5879b8db84d92be8f47984a4674de3c3
UsmanSofa <usmansofawala70@gmail.com>
2026-09-03 10:09:05 +0530

    Function Programs

---
+++ functions/NANR.c
@@ -0,0 +1,10 @@
+//Function with no Arguement and Return Valur
+#include<stdio.h>
+void greetUser(){
+    printf("Hello Programmer!!!");
+    return; 
+}
+int main(){
+    greetUser();
+    return 0;
+}
